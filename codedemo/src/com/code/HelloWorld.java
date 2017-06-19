2a2def1fdb7fab291bbaca1ff4c22966e70f0abe
Administrator <Administrator@USER-20150706QN>
2017-06-19 10:34:56 +0800

    66666666

--- codedemo/src/com/code/HelloWorld.java
+++ codedemo/src/com/code/HelloWorld.java
@@ -7,10 +7,11 @@ public class HelloWorld {
 		
 		System.out.println("222222222222");
 		
-		
 		System.out.println("3333333333333");
 		
 		System.out.println("5555555555555");
+		
+		System.out.println("6666666666666666");
 		
 	}
 }
